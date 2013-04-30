92f64460d1ce00501990b28f826aa6bccbcc8751
ygabo <yelnil@gmail.com>
2013-04-29 19:45:07 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = '80f66b841bd7f85793b029d27d283796feff454523f28faf14f6e020de7102ab7aaf24c324c9eeb0485ac029e9b4d5483d39be887cad2f53cbdd5263ea4a63f9'
